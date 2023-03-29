77aa9a2663ddaa64df92e366b37d53202656dc03
JhonHurtado <jhonse2001@gmail.com>
2023-03-29 08:40:16 -0500

    cambio de api

--- src/database.js
+++ src/database.js
@@ -2,14 +2,11 @@ import mysql from "mysql2/promise";
 import { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } from "./config.js";
 
 const pool = mysql.createPool({
-
   user: DB_USER,
   password: DB_PASSWORD,
-  port: DB_PORT,
   host: DB_HOST,
+  port: DB_PORT,
   database: DB_NAME,
-
-
 });
 
 const connecToDtabase = async () => {
