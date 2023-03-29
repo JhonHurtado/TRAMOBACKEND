1222530367ae678843d19284655631949a3e2159
JhonHurtado <jhonse2001@gmail.com>
2023-03-28 22:05:29 -0500

    env

--- src/database.js
+++ src/database.js
@@ -2,10 +2,14 @@ import mysql from "mysql2/promise";
 import { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } from "./config.js";
 
 const pool = mysql.createPool({
+
   user: DB_USER,
   password: DB_PASSWORD,
+  port: DB_PORT,
   host: DB_HOST,
   database: DB_NAME,
+
+
 });
 
 const connecToDtabase = async () => {
